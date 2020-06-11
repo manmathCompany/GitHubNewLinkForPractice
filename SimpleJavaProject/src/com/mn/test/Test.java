a0a86100d92f2cbab1ea84a7606fa17fd0b8778a
TL <TL@gmail.com>
2020-06-11 23:07:50 +0530

    added main method and calling addmethod

--- SimpleJavaProject/src/com/mn/test/Test.java
+++ SimpleJavaProject/src/com/mn/test/Test.java
@@ -1,5 +1,14 @@
 package com.mn.test;
 
+import com.mn.model.CalCulator;
+
 public class Test {
 
+	public static void main(String[] args) {
+		
+		CalCulator c=new CalCulator();
+		
+		int result=c.add(10, 20);
+		System.out.println(result);
+	}
 }
